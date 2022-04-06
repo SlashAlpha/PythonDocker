88bffa081a60bb0e42022f7e6562b26957836218
Clément Duprat <jobclement@hotmail.fr>
2022-04-06 16:19:57 +0200

    Python server with flask and Docker File

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+# pip freeze > requirements.txt !!!
+app = Flask(__name__)
+
+
+def be_polite(fn):
+    def wrapper():
+        return "Have a nice  day. "+fn()
+
+    return wrapper
+
+
+@be_polite
+def greet():
+    return "My name is Colt."
+
+
+@be_polite
+def rage():
+    return "GRrrr"
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return greet()
+
+
+if __name__ == '__main__':
+    app.run()
